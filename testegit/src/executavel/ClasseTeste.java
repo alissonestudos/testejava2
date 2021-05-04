9aa9a78c4098e44915f4d9dda54f66e0254531ca
Alisson Teixeira <alisson2805@outlook.com>
2021-05-04 18:40:03 -0300

    teste commit 3

--- testegit/src/executavel/ClasseTeste.java
+++ testegit/src/executavel/ClasseTeste.java
@@ -5,6 +5,7 @@ public class ClasseTeste {
 	public static void main(String[] args) {
 		// TODO Auto-generated metgit satuahod stub
 		System.out.println("teste mudança git 2");
+		System.out.println("teste mudança git 3");
 	}
 
 }
