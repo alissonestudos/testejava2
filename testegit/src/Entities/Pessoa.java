6ad27feaedc9e01ee67b45df7edd454e72eef215
Alisson Teixeira <alisson2805@outlook.com>
2021-05-05 10:35:02 -0300

    Instalação do Lombok

---
+++ testegit/src/Entities/Pessoa.java
@@ -0,0 +1,11 @@
+package Entities;
+
+import lombok.Data;
+
+public @Data class Pessoa {
+	
+	private final String nome;
+	private final double salario;
+	private String cpf;
+
+}
--- testegit/src/executavel/ClasseTeste.java
+++ testegit/src/executavel/ClasseTeste.java
@@ -1,11 +1,17 @@
 package executavel;
 
+import Entities.Pessoa;
+
 public class ClasseTeste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated metgit satuahod stub
 		System.out.println("teste mudança git 2");
 		System.out.println("teste mudança git 3");
+		
+		Pessoa pessoa1 = new Pessoa("Alisson", 0);
+		
+		System.out.println(pessoa1.toString());
 	}
 
 }
